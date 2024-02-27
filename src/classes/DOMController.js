ee346bd419767f3880b848bb405ff6f1b15b900a
Tae Woo Kim <taewookim02@gmail.com>
2024-02-27 18:40:43 +0900

    Add modal skeleton

--- src/classes/DOMController.js
+++ src/classes/DOMController.js
@@ -4,13 +4,9 @@ export class DOMController {
     const content = document.querySelector("#content");
     const nav = document.querySelector(".nav");
     const header = document.querySelector(".header");
+    const container = document.querySelector(".container");
 
-    // add project button
-    const projectBtn = document.createElement("button");
-    projectBtn.classList.add("project__btn");
-    projectBtn.textContent = "+";
-    header.appendChild(projectBtn);
-
+    // reset content
     nav.innerHTML = `
       <h2>Projects</h2>
       <div class="nav__projects"></div>
@@ -19,6 +15,37 @@ export class DOMController {
       <h2>Todos</h2>
       <div class="todos__container"></div>
     `;
+
+    // add project button
+    const projectBtn = document.createElement("button");
+    projectBtn.classList.add("project__btn");
+    projectBtn.textContent = "+";
+    header.appendChild(projectBtn);
+    projectBtn.addEventListener("click", (e) => {
+      e.preventDefault();
+    });
+
+    DOMController.renderModal(container);
+  }
+
+  static renderModal(container) {
+    // modal overlay
+    const overlayModal = document.createElement("div");
+    overlayModal.classList.add("modal__overlay");
+    container.appendChild(overlayModal);
+
+    // project modal
+    const projectModal = document.createElement("div");
+    projectModal.classList.add("project__modal");
+    projectModal.innerHTML = `
+  <form class="project__form">
+    <h2>Add Project</h2>
+    <button class="project__close">X</button>
+    <input type="text" class="project__name" placeHolder="Project Name:" />
+    <button class="project__submit">Submit</button>
+  </form>
+`;
+    container.appendChild(projectModal);
   }
 
   // render all projects
