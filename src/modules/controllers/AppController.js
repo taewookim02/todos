409590ce18b3272aefd21b1dec949ebb7ec09b58
Tae Woo Kim <taewookim02@gmail.com>
2024-03-14 18:44:39 +0900

    Reinit project

---
+++ src/modules/controllers/AppController.js
@@ -0,0 +1,11 @@
+import { StorageController } from "./StorageController";
+
+export class AppController {
+  constructor() {
+    // this.StorageController = new StorageController();
+  }
+
+  init() {
+    console.log("hello from appcontroller");
+  }
+}
---
+++ src/modules/controllers/StorageController.js
@@ -0,0 +1,13 @@
+export default class StorageController {
+  constructor() {
+    this.storage = window.localStorage;
+  }
+
+  get(key) {
+    return this.storage.getItem(key);
+  }
+
+  set(key, value) {
+    return this.storage.setItem(key, value);
+  }
+}
